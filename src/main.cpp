9b635ecba94375cd4b251bc19859ddaf1ee296fe
Steven Upton <supton.sz@gmail.com>
2021-10-23 16:53:02 +0100

    Moved platform specific definitions to main.cpp instead of having to define them in compiler options. Added DEBUG definition

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
+#ifndef NDEBUG
+	#define DEBUG
+#endif
+
 #if WIN32 
-#include "platform/windows/entry.cpp"
+	#define UNICODE
+	#define _USE_MATH_DEFINES
+	#include "platform/windows/entry.cpp"
 #else
-#error "No platform macro defined. Available platforms are: WIN32"
+	#error "No platform macro defined. Available platforms are: WIN32"
 #endif
