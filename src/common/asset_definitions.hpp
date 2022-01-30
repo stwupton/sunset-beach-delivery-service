255dc572f6f121b9e2bb36fa9bbb743a0d4b2dc0
Ross Townsend <rossithebest@gmail.com>
2022-01-30 13:10:18 +0000

    Making amendments as per pull request comments

--- src/common/asset_definitions.hpp
+++ src/common/asset_definitions.hpp
@@ -10,12 +10,12 @@ enum class TextureAssetId : u8 {
 };
 
 namespace _TextureAssetFileNames {
-	const wchar_t* ship = L"assets/img/ship.png";
-	const wchar_t* enemyShip = L"assets/img/enemy_ship.png";
-	const wchar_t* background = L"assets/img/starry_background.jpg";
+	const wchar_t *ship = L"assets/img/ship.png";
+	const wchar_t *enemyShip = L"assets/img/enemy_ship.png";
+	const wchar_t *background = L"assets/img/starry_background.jpg";
 };
 
-const wchar_t* textureNames[] = {
+const wchar_t *textureNames[] = {
 	_TextureAssetFileNames::ship,
 	_TextureAssetFileNames::enemyShip,
 	_TextureAssetFileNames::background
@@ -25,18 +25,17 @@ enum class SoundAssetId : u8 {
 	left,
 	right,
 	stereo,
-	doomDeath,
-	_length
+	doomDeath
 };
 
 namespace _SoundAssetFileNames {
-	const wchar_t* left = L"assets/music/Left.wav";
-	const wchar_t* right = L"assets/music/Right.wav";
-	const wchar_t* stereo = L"assets/music/Stereo.wav";
-	const wchar_t* doomDeath = L"assets/music/sound1.wav";
+	const wchar_t *left = L"assets/music/Left.wav";
+	const wchar_t *right = L"assets/music/Right.wav";
+	const wchar_t *stereo = L"assets/music/Stereo.wav";
+	const wchar_t *doomDeath = L"assets/music/sound1.wav";
 };
 
-const wchar_t* soundNames[] = {
+const wchar_t *soundNames[] = {
 	_SoundAssetFileNames::left,
 	_SoundAssetFileNames::right,
 	_SoundAssetFileNames::stereo,
@@ -45,14 +44,14 @@ const wchar_t* soundNames[] = {
 
 enum class MusicAssetId : u8 {
 	mars,
-	_length,
 	none
 };
 
 namespace _MusicAssetFileNames {
-	const wchar_t* mars = L"assets/music/mars.wav";
+	// Source: https://www.free-stock-music.com/arthur-vyncke-approaching-mars.html
+	const wchar_t *mars = L"assets/music/mars.wav";
 };
 
-const wchar_t* musicNames[] = {
+const wchar_t *musicNames[] = {
 	_MusicAssetFileNames::mars
 };
