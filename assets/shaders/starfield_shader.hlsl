fd1aee14f323dd6ae317f00f261e47ce88cea019
Steven Upton <supton.sz@gmail.com>
2022-05-30 11:33:19 +0100

    Finally figured out what was wrong with the starfield shader

--- assets/shaders/starfield_shader.hlsl
+++ assets/shaders/starfield_shader.hlsl
@@ -12,30 +12,34 @@ PixelInput vertex(VertexInput input) {
 	return output;
 }
 
-float hash21(float2 p) {
-    p = frac(p * float2(123.34f, 456.21f));
-    p += dot(p, p + 45.32f);
-    return frac(p.x * p.y);
+float hash21(int2 input) {
+    float2 result = frac(input * float2(123.34f, 456.21f));
+    result += dot(result, result + 45.32f);
+    return frac(result.x * result.y);
+}
+
+float star(float2 uv) {
+	float distance = length(uv);
+	float result = 0.05f / distance;
+	result *= smoothstep(0.5f, 0.2f, distance);
+
+	return result;
 }
 
 float4 pixel(PixelInput input) : SV_TARGET {
-	float2 uv = (input.position.xy - 0.5f * float2(1920.0f, 1080.0f)) / 1080.0f;
+	float2 uv = (input.position.xy - float2(1920.0f, 1080.0f) * 0.5f) / 1080.0f;
 	uv *= 30.0f;
 
 	float3 color = float3(0.0f, 0.0f, 0.0f);
 
 	float2 gv = frac(uv) - 0.5f;
-	float2 id = floor(uv);
+	int2 id = floor(uv);
 
 	for (int y = -1; y <= 1; y++) {
 		for (int x = -1; x <= 1; x++) {
-			float2 offset = float2(x, y);
-			float n = hash21(id - offset);
-
-			float j = length(gv - offset - float2(n, frac(n * 10.0f)));
-			float light = 0.05f / j;
-			light *= smoothstep(0.5, 0.2f, j);
-			color += float3(light, light, light);
+			int2 offset = float2(x, y);
+			float n = hash21(id + offset);
+			color += star(gv - offset - float2(n, frac(n * 10.0f)));
 		}
 	}
 
