a294c806ee6c9620edcf7c35cab32ab3fdc3776b
Steven Upton <supton.sz@gmail.com>
2022-06-01 19:23:46 +0100

    starfield flare

--- assets/shaders/starfield_shader.hlsl
+++ assets/shaders/starfield_shader.hlsl
@@ -12,15 +12,29 @@ PixelInput vertex(VertexInput input) {
 	return output;
 }
 
+float2x2 rotate(float x) {
+	float s = sin(x);
+	float c = cos(x);
+	return float2x2(c, -s, s, c);
+}
+
 float hash21(int2 input) {
     float2 result = frac(input * float2(123.34f, 456.21f));
     result += dot(result, result + 45.32f);
     return frac(result.x * result.y);
 }
 
-float star(float2 uv) {
+float star(float2 uv, float flare) {
 	float distance = length(uv);
 	float result = 0.05f / distance;
+
+	float rays = max(0.0f, 1.0f - abs(uv.x * uv.y * 1000.));
+	result += rays * flare;
+
+	uv = mul(uv, rotate(3.1415f / 4.0f));
+	rays = max(0.0f, 1.0f - abs(uv.x * uv.y * 1000.));
+	result += rays * flare;
+
 	result *= smoothstep(0.5f, 0.2f, distance);
 
 	return result;
@@ -28,7 +42,7 @@ float star(float2 uv) {
 
 float4 pixel(PixelInput input) : SV_TARGET {
 	float2 uv = (input.position.xy - float2(1920.0f, 1080.0f) * 0.5f) / 1080.0f;
-	uv *= 30.0f;
+	uv *= 50.0f;
 
 	float3 color = float3(0.0f, 0.0f, 0.0f);
 
@@ -37,9 +51,12 @@ float4 pixel(PixelInput input) : SV_TARGET {
 
 	for (int y = -1; y <= 1; y++) {
 		for (int x = -1; x <= 1; x++) {
-			int2 offset = float2(x, y);
+			int2 offset = int2(x, y);
 			float n = hash21(id + offset);
-			color += star(gv - offset - float2(n, frac(n * 10.0f)));
+			float size = frac(n * 345.567f);
+
+			float starColor = star(gv - offset - float2(n, frac(n * 10.0f)), smoothstep(0.6f, 1.0f, size));
+			color += starColor * size;
 		}
 	}
 
